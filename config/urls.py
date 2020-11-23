902c0a759151eae1107e1b60cf4fb59b8ed6b311
sho25052007 <sho25052007@gmail.com>
2020-11-23 19:57:15 +0000

    forms and user accounts

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+# config/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')), # must be below auth (above)
+    path('', include('blog.urls')),
+]
